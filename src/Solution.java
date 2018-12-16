84dd8b32f3ade4ccd3abdbd2a47bfe960abbaa1c
SQ <saber1010137@gmail.com>
2018-12-16 15:40:27 +0800

    Algorithms

---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Solution
+{
+    public int singleNumber(int[] A) {
+
+        int result = 0;
+
+        for(int i = 0; i < A.length; i++)
+        {
+            result ^= A[i];
+        }
+        return result;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Solution my = new Solution();
+
+        int[] a = {1,0,1};
+
+        System.out.println(my.singleNumber(a));
+    }
+
+
+}
